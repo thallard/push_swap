cfaa1b37687d81b90a72073ae222ff31e080a465
thallard <thallard@student.42lyon.fr>
2021-03-03 16:45:10 +0100

    Check all integers with atoi/itoa for parsing

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: thallard <thallard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 14:58:48 by thallard          #+#    #+#             */
-/*   Updated: 2021/03/03 16:29:13 by thallard         ###   ########lyon.fr   */
+/*   Updated: 2021/03/03 16:42:40 by thallard         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int		ft_contains_alpha(char *num)
 
 	i = -1;
 	while (num[++i])
-		if (!ft_isdigit(num[i]))
+		if (!ft_isdigit(num[i]) && num[i] != '-')
 			return (0);
 	return (1);
 }
@@ -28,11 +28,12 @@ int		ft_check_doubles(t_global *g, int argc)
 	int		i;
 	int		j;
 
-	j = 0;
+	j = -1;
 	i = -1;
-	while (j++ < argc - 2 && (i = -1) < 0)
-		while (g->a[++i])
-			if (!ft_strncmp(g->a[i], g->a[j], ft_strlen(g->a[i])))
+	while (++j < argc && (i = -1) < 0)
+		while (g->a[++i] && g->a[j])
+			if (i != j)
+			if (!ft_strncmp(g->a[i], g->a[j], 11))
 				return (0);
 	return (1);
 }
@@ -46,9 +47,11 @@ int		ft_init_stack(t_global *g, int argc, char **argv)
 	i = 0;
 	g->a = malloc(sizeof(char *) * argc);
 	while (++i < argc && argv[i])
+	{
 		if (!ft_contains_alpha(argv[i]))
 			return (0);
-		g->a[++j] = argv[i];
+		g->a[++j] = ft_itoa(ft_atoi(argv[i]));
+	}
 	g->a[++j] = NULL;
 	if (!ft_check_doubles(g, argc))
 		return (0);
